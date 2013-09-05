9882952bbcd47ff4794dbf10d10cd0a72ad7f6a8
Parlakov <gparlakov@abv.bg>
2013-09-05 12:43:53 -0700

    2.WebForms Intro Downloaded full bootstrap :) - works Removed unused font added JS booststrap

--- 2.WebForms Intro/Hello/hello.aspx.designer.cs
+++ 2.WebForms Intro/Hello/hello.aspx.designer.cs
@@ -65,5 +65,32 @@ namespace HelloASP {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl printFromCode;
+        
+        /// <summary>
+        /// ButtonOK control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonOK;
+        
+        /// <summary>
+        /// events control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox events;
+        
+        /// <summary>
+        /// AssemblyLocation control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl AssemblyLocation;
     }
 }
